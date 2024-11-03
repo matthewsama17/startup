1801d6ea5985d66377afe9a9fc7fa0f73535f2af
Matthew Herron <m.3herron@gmail.com>
2024-11-02 22:23:08 -0600

    Added header and footer to React

--- src/app.jsx
+++ src/app.jsx
@@ -2,5 +2,31 @@ import React from 'react';
 import './app.css';
 
 export default function App() {
-  return <div className='body'>App will display here</div>;
+  return (
+    <div className='body'>
+      <header>
+        <ul className="navigation-menu">
+          <li><a href="index.html">Game</a></li>
+          <div></div>
+          <li><a href="login.html">Login</a></li>
+          <div></div>
+          <li><a href="scores.html">Scores</a></li>
+        </ul>
+        <div className="title">
+          <p id="title-1">WEBSITE</p>
+          <p id="title-2">that</p>
+          <p id="title-3">BEATS YOU</p>
+          <p id="title-4">at</p>
+          <p id="title-5">TIC TAC TOE</p>
+        </div>
+      </header>
+
+      <main>App components go here</main>
+
+      <footer>
+        <p id="Author">Matthew Herron</p>
+        <a href="https://github.com/matthewsama17">His github</a>
+      </footer>
+    </div>
+  );
 }
