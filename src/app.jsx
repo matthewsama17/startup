f29bcb5d70f1a13301d3ce539b18a1e5fd4ffdb2
Matthew Herron <m.3herron@gmail.com>
2024-11-02 22:39:56 -0600

    React Routing implemented

--- src/app.jsx
+++ src/app.jsx
@@ -1,32 +1,48 @@
 import React from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+
 import './app.css';
+import { Game } from './game/game';
+import { Login } from './login/login';
+import { Scores } from './scores/scores';
 
 export default function App() {
   return (
-    <div className='body'>
-      <header>
-        <ul className="navigation-menu">
-          <li><a href="index.html">Game</a></li>
-          <div></div>
-          <li><a href="login.html">Login</a></li>
-          <div></div>
-          <li><a href="scores.html">Scores</a></li>
-        </ul>
-        <div className="title">
-          <p id="title-1">WEBSITE</p>
-          <p id="title-2">that</p>
-          <p id="title-3">BEATS YOU</p>
-          <p id="title-4">at</p>
-          <p id="title-5">TIC TAC TOE</p>
-        </div>
-      </header>
+    <BrowserRouter>
+      <div className='body'>
+        <header>
+          <ul className="navigation-menu">
+            <li><NavLink to="">Game</NavLink></li>
+            <div></div>
+            <li><NavLink to="login">Login</NavLink></li>
+            <div></div>
+            <li><NavLink to="scores">Scores</NavLink></li>
+          </ul>
+          <div className="title">
+            <p id="title-1">WEBSITE</p>
+            <p id="title-2">that</p>
+            <p id="title-3">BEATS YOU</p>
+            <p id="title-4">at</p>
+            <p id="title-5">TIC TAC TOE</p>
+          </div>
+        </header>
 
-      <main>App components go here</main>
+        <Routes>
+          <Route path='/' element={<Game />} exact />
+          <Route path='/login' element={<Login />} />
+          <Route path='/scores' element={<Scores />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
 
-      <footer>
-        <p id="Author">Matthew Herron</p>
-        <a href="https://github.com/matthewsama17">His github</a>
-      </footer>
-    </div>
+        <footer>
+          <p id="Author">Matthew Herron</p>
+          <a href="https://github.com/matthewsama17">His github</a>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
+}
+
+function NotFound() {
+  return <main>404: Return to sender. Address unknown.</main>;
 }
---
+++ src/game/game.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Game() {
+  return (
+    <main>
+      <div>game displayed here</div>
+    </main>
+  );
+}
