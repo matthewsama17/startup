61f71039c300ec9a3705056fbf9014f24815a36f
Matthew Herron <m.3herron@gmail.com>
2024-11-09 23:25:23 -0700

    Added the board

--- src/game/game.jsx
+++ src/game/game.jsx
@@ -1,14 +1,55 @@
 import React from 'react';
+import { useState } from 'react';
 import './game.css';
 
 export function Game() {
   return (
     <main className="game-main">
       <p className="game-p">Player: <span id="player-name">Mystery Player</span></p>
-      <img src="/tictactoe.png" width={400} />
+      <Board />
       <div>
         <button className="game-button">RESTART</button>
       </div>
     </main>
+  );
+}
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    if(value == null) {
+      setValue('X');
+    }
+    else {
+      setValue(null);
+    }
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>{value}</button>
+  );
+}
+
+
+function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
   );
 }
