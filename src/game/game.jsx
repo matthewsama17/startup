ab1dc2d511f573f6797287b5bdda2785bd81896c
Matthew Herron <m.3herron@gmail.com>
2024-11-22 18:53:52 -0700

    Implemented ResetButton

--- src/game/game.jsx
+++ src/game/game.jsx
@@ -7,53 +7,68 @@ export function Game() {
     <main className="game-main">
       <p className="game-p">Player: <span id="player-name">Mystery Player</span></p>
       <Board />
-      <div>
-        <button className="game-button">RESTART</button>
-      </div>
     </main>
   );
 }
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    if(value == null) {
-      fetch('/letter')
-        .then((response) => response.json())
-        .then((response) => {
-          setValue(response["letter"]);
-        });
-    }
-    else {
-      setValue(null);
-    }
-  }
-
+function Square({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={handleClick}>{value}</button>
+    <button className="square" onClick={onSquareClick}>{value}</button>
   );
 }
 
+function ResetButton({ onButtonClick }) {
+  return (
+    <div>
+      <button className="game-button" onClick={onButtonClick}>RESTART</button>
+    </div>
+  )
+}
 
 function Board() {
+  const [squares, setSquares] = useState([Array(9).fill(null)]);
+
+  function handleClick(i) {
+    const newSquares = squares.slice();
+
+    if(squares[i] == null) {
+      fetch('/letter')
+        .then((response) => response.json())
+        .then((response) => {
+          newSquares[i] = response["letter"];
+          setSquares(newSquares);
+        });
+    }
+    else {
+      newSquares[i] = null;
+      setSquares(newSquares);
+    }
+  }
+
+  function onReset() {
+    setSquares([Array(9).fill(null)]);
+  }
+
   return (
-    <div className="board">
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+    <>
+      <div className="board">
+        <div className="board-row">
+          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        </div>
+        <div className="board-row">
+          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        </div>
+        <div className="board-row">
+          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        </div>
       </div>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-    </div>
+      <ResetButton onButtonClick={onReset} />
+    </>
   );
 }
